29ecb195f5662d947666bb28739bfa4fdc8a5aef
Irek <general157@mail.ru>
2022-12-19 20:03:46 +0300

    simplifyPath added

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sln = new Solution();
+        System.out.println(sln.simplifyPath("//home/foo"));
+
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.ArrayDeque;
+
+class Solution {
+    public String simplifyPath(String path) {
+        String[] pathArr = path.split("/");
+        ArrayDeque<String> deque = new ArrayDeque<>();
+        for (String s : pathArr) {
+            if (s.equals("..") && !deque.isEmpty()) {
+                deque.removeLast();
+            } else if (!s.equals("") && !s.equals("..") && !s.equals(".")) {
+                deque.add(s);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("/");
+        while (!deque.isEmpty()) {
+            sb.append(deque.poll());
+            if (!deque.isEmpty()) {
+                sb.append("/");
+            }
+        }
+        return sb.toString();
+    }
+}
