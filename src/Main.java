00f7548c926664bbf378ea3433c99614219a1b25
Irek <general157@mail.ru>
2022-12-19 23:20:16 +0300

    getIslandArea added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         Solution sln = new Solution();
-        System.out.println(sln.simplifyPath("//home/foo"));
+        int[][] field = {
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        };
 
+        int maxSize = sln.maxAreaOfIsland(field);
+        System.out.println(maxSize);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -21,4 +21,44 @@ class Solution {
         }
         return sb.toString();
     }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int size;
+        int maxSize = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    size = getIslandArea(i, j, grid);
+                    if (size > maxSize) {
+                        maxSize = size;
+                    }
+                }
+            }
+        }
+        return maxSize;
+    }
+
+    private int getIslandArea(int row, int col, int[][] grid) {
+        int area = 0;
+        int[][] offsets = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int[] curPoint;
+        ArrayDeque<int[]> stack = new ArrayDeque<>();
+        stack.push(new int[]{row, col});
+        grid[row][col] = 0;
+        while (!stack.isEmpty()) {
+            curPoint = stack.pop();
+            area++;
+            for (int[] offset : offsets) {
+                int newRow = curPoint[0] + offset[0];
+                int newCol = curPoint[1] + offset[1];
+                if ((0 <= newRow && newRow < grid.length) &&
+                        (0 <= newCol && newCol < grid[0].length) &&
+                        (grid[newRow][newCol] != 0)) {
+                    stack.push(new int[]{newRow, newCol});
+                    grid[newRow][newCol] = 0;
+                }
+            }
+        }
+        return area;
+    }
 }
